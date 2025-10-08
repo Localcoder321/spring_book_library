d83500d111f9cb02562c6dd41b35cb8edaa4647f
Murod <abdumajidov.murod@sarkor.uz>
2025-10-08 23:17:45 +0500

    first commit

---
+++ src/test/java/dev/localcoder/springbooklibrary/SpringBookLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.localcoder.springbooklibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBookLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
