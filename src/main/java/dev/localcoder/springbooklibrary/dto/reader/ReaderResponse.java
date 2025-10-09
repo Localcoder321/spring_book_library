7a20e226690aa161bc152e0ff8172999dc99b672
Murod <abdumajidov.murod@sarkor.uz>
2025-10-09 16:14:40 +0500

    minor changes

--- src/main/java/dev/localcoder/springbooklibrary/dto/reader/ReaderResponse.java
+++ src/main/java/dev/localcoder/springbooklibrary/dto/reader/ReaderResponse.java
@@ -1,8 +1,10 @@
 package dev.localcoder.springbooklibrary.dto.reader;
 
+import dev.localcoder.springbooklibrary.dto.rental.RentalResponse;
 import lombok.Data;
 
 import java.time.Instant;
+import java.util.List;
 
 @Data
 public class ReaderResponse {
