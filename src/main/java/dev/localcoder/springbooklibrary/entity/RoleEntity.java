48d3bd731fc210ce07480d29482650c180dcb2d1
Murod <abdumajidov.murod@sarkor.uz>
2025-11-11 19:49:46 +0500

    added roles to readers

---
+++ src/main/java/dev/localcoder/springbooklibrary/entity/RoleEntity.java
@@ -0,0 +1,21 @@
+package dev.localcoder.springbooklibrary.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RoleEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String name;
+}
---
+++ src/main/java/dev/localcoder/springbooklibrary/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package dev.localcoder.springbooklibrary.repository;
+
+import dev.localcoder.springbooklibrary.entity.RoleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
+    Optional<RoleEntity> findByName(String name);
+}
