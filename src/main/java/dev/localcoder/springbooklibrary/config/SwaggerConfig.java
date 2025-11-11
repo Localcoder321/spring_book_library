d219007f2526c596bcceeef5040d1e7fbe2b5995
Murod <abdumajidov.murod@sarkor.uz>
2025-11-11 19:51:33 +0500

    added dependencies for authorization and made changes in swagger config and removed the custom config of flyway

--- src/main/java/dev/localcoder/springbooklibrary/config/SwaggerConfig.java
+++ src/main/java/dev/localcoder/springbooklibrary/config/SwaggerConfig.java
@@ -2,16 +2,29 @@ package dev.localcoder.springbooklibrary.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+    final String securityScheme = "bearerAuth";
+
     @Bean
     public OpenAPI apiInfo() {
         return new OpenAPI().info(
                 new Info().title("Library Service Api")
                         .description("REST API for book and reader management")
-                        .version("1.0.0"));
+                        .version("1.0.0")).addSecurityItem(new SecurityRequirement()
+                .addList(securityScheme))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes(securityScheme,
+                new SecurityScheme()
+                .name(securityScheme)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .description("Enter JWT token with Bearer prefix")));
     }
 }
